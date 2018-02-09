6b28cb8eaef97a1589cb9e6e70a7b4ce60424a03
Christopher Ashby <ashbyca@users.noreply.github.com>
2018-02-09 10:31:32 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@ sitemap:
 ---
 
 $(document).ready(function () {
+  $('a.blog-button').click(function (e) {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  })
+
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
@@ -23,22 +35,9 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
-  $('a.about-button').click(function (e) {
-  if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-  if (currentWidth < 960) {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-    $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-    $('.panel-cover').css('max-width', currentWidth)
-    $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-   }
-   })
-                  
-   if (window.location.hash && window.location.hash == '#about') {
-     $('.panel-cover').addClass('panel-cover--collapsed')
-   }
-    $('a.blog-button').click(function (e) {
+})
+
+$('a.about-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
@@ -49,4 +48,7 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
-})
+
+if (window.location.hash && window.location.hash == '#about') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
